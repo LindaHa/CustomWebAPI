475522b76c3ccd690b55042e6146b93c49691b3c
LindaHa <lindaelvira.h@gmail.com>
2016-08-31 16:36:56 +0200

    GetAllUsers, RemoveRole

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -11,7 +11,9 @@ using CMS.DataEngine;
 using System.Data;
 using System.Linq;
 using System.Collections.Generic;
+using CMS.MembershipProvider;
 using CMS.Membership;
+using Newtonsoft.Json.Linq;
 
 namespace CustomWebApi
 {
@@ -21,16 +23,47 @@ namespace CustomWebApi
         [Route("kenticoapi/users")]
         public HttpResponseMessage GetAllUsers()
         {
-            try{
+            CMSRoleProvider cmsRoleProvider = new CMSRoleProvider();
+            try
+            {
                 DataSet users = UserInfoProvider.GetFullUsers("", "UserID ASC");
                 List<Object> usersList = users.Tables[0].AsEnumerable().Select(
-                        dataRow => new {
+                        dataRow => new
+                        {
                             UserId = dataRow.Field<int>("userid"),
                             FirstName = dataRow.Field<string>("firstname"),
                             Surname = dataRow.Field<string>("lastname"),
                             Email = dataRow.Field<string>("email"),
+                            Username = dataRow.Field<string>("username"),
+                            //UsrName = dataRow.Field<string>("nickname"),
+                            Roles = cmsRoleProvider.GetRolesForUser(dataRow.Field<string>("username")),
                         }).ToList<Object>();
                 return Request.CreateResponse(HttpStatusCode.OK, new { usersList = usersList });
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.ServiceUnavailable, new { errorMessage = e.Message });
+            }
+        }
+
+        [HttpPost]
+        [Route("kenticoapi/users/remove-users-from-roles")]
+        public HttpResponseMessage RemoveUsersFromRoles([FromBody]JObject postData)
+        {
+            string[] usernames, roleNames;
+            try
+            {
+                usernames = postData["usernames"].ToObject<string[]>();
+                roleNames = postData["roleNames"].ToObject<string[]>();
+            }catch(Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, new { errorMessage = e.Message });
+            }
+            CMSRoleProvider cmsRoleProvider = new CMSRoleProvider();
+            try
+            {
+                cmsRoleProvider.RemoveUsersFromRoles(usernames, roleNames);
+                return Request.CreateResponse(HttpStatusCode.OK, new { });
             }
             catch (Exception e)
             {
